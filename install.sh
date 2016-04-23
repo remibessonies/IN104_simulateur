b99ea7aa1aa8c793e5ade68d52e032d067ad25bf
Clément <masson.cle@gmail.com>
2016-04-24 01:47:02 +0200

    Make project installable with python setup.py install

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 setup.py install
+echo "\nIN104_simulateur successfully installed"
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+    
+setup(name='IN104_simulateur',
+    version='1.0',
+    description='Game simulator for checkers and checkers-like games',
+    author='Clement Masson',
+    author_email='masson.cle@gmail.com',
+    url='https://github.com/clement-masson/IN104_simulateur',
+    license='MIT',
+    packages = ['IN104_simulateur']
+)
