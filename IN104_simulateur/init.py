6347d609ec41975eb3801e6ef68f2accb4ee2c8b
Clément <masson.cle@gmail.com>
2017-05-02 16:14:23 +0200

    renommage init.py

--- IN104_simulateur/init.py
+++
@@ -1,7 +0,0 @@
-from .boardState import BoardState
-from .gameState import GameState
-from . import cell as Cell
-from .move import Move
-
-from .game import  Game
-from .manualBrain import ManualBrain
